27db4fe100447ee39fb66cca1a5cf91465017272
Luc Gaitskell <me@lucg.xyz>
2025-06-01 18:31:46 -0400

    Switch productivity MAS apps

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -39,8 +39,9 @@ in
     # you may receive an error message "Redownload Unavailable with This Apple ID".
     # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
     masApps = {
-      "1password" = 1333542190;
       "wireguard" = 1451685025;
+      "craft" = 1487937127;
+      "microsoft-outlook" = 985367838;
     };
   };
 
