a27b4732a4c3a45d31c8e44239e5215757f21833
Luc Gaitskell <me@lucg.xyz>
2025-09-07 13:31:14 -0400

    Adjust dock

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -75,14 +75,13 @@ in
     enable = true;
     username = user;
     entries = [
+    { path = "/Applications/Launchpad.app/"; }
     { path = "/Applications/Slack.app/"; }
     { path = "/System/Applications/Messages.app/"; }
-    { path = "/System/Applications/Facetime.app/"; }
     { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
     { path = "/System/Applications/Music.app/"; }
     { path = "/System/Applications/News.app/"; }
     { path = "/System/Applications/Photos.app/"; }
-    { path = "/System/Applications/Photo Booth.app/"; }
     { path = "/System/Applications/TV.app/"; }
     { path = "/System/Applications/Home.app/"; }
     {
